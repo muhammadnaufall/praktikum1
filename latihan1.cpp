832652c295647c80a304f747f0267c93bc4628b1
muhammadnaufall <muhammadnaufal806@gmail.com>
2018-10-21 10:12:51 +0700

    file sudah di rubah latihan1.cpp

---
+++ latihan1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+int p, l, luas;
+cout << "Masukan Panjang: ";
+cin >> p;
+cout << "Masukan Lebar: ";
+cin >> l;
+
+luas = p * l;
+
+
+cout << "Luas Persegi panjang adalah: " << luas << endl;
+
+return 0;
+
+}
